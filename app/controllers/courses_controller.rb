8e9bfbb8cd2da380a6c760384f0195284845399b
Mark Merritt <markmerritt804@gmail.com>
2019-10-22 10:22:21 -0400

    Add courses new/create/show actions

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -2,4 +2,29 @@ class CoursesController < ApplicationController
   def index
     @courses = Course.all
   end
+
+  def show
+    @course = Course.find(params[:id])
+  end
+
+  def new
+    @course = Course.new
+  end
+
+  def create
+    @course = Course.new(course_params)
+
+    if @course.save
+      redirect_to @course 
+    else
+      render :new
+    end
+  end
+
+
+  private
+
+  def course_params
+    params.require(:course).permit(:title)
+  end
 end
--- spec/features/courses_spec.rb
+++ spec/features/courses_spec.rb
@@ -1,6 +1,7 @@
 require "rails_helper"
 
 describe "navigation" do 
+  #--------------------- Index ---------------------#
   describe "index" do 
     before do 
       @course1 = FactoryBot.create(:course)
@@ -11,6 +12,16 @@ describe "navigation" do
       visit courses_path
       
       expect(page).to have_content(/#{@course1.title}|#{@course2.title}/)
+    end
+  end
+
+  #--------------------- New ---------------------#
+  describe "new" do 
+    it "can be created" do 
+      visit new_course_path
+      fill_in "title-field", with: "Some Title"
+
+      expect{ click_button "create-new-course-btn" }.to change{ Course.count }.by(1)
     end
   end
 end
