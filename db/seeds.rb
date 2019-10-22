2c23ed33c350230e3899c4d0884cfe55b42d9208
Mark Merritt <markmerritt804@gmail.com>
2019-10-22 09:46:30 -0400

    Seed DB with courses

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,27 @@
 require "faker"
 
-# Reset
+#------------------ Reset ------------------#
+Course.delete_all
 Admin.delete_all
 
-# Create an Admin
+
+
+#------------------ Create Admin ------------------#
+
 Admin.create!(
   email: "admin@example.com",
   password: "helloworld"
 )
 
 puts "#{Admin.count} admin users created"
+
+
+#------------------ Create Courses ------------------#
+
+5.times do 
+  Course.create!(
+    title: Faker::Educator.course_name
+  )
+end
+
+puts "#{Course.count} courses created"
